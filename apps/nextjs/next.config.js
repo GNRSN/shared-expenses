226845d0828ed643514c961a76c354053a4c52fc
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-07-02 23:46:25 +0200

    enable react compiler

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -20,6 +20,10 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default config;
