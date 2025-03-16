ea9fef559fb562869b6164d18c0edc1b134a0fc5
GNRSN <9020004+GNRSN@users.noreply.github.com>
2025-03-16 14:07:46 +0100

    add form submit button component

--- packages/ui/src/components/form.tsx
+++ packages/ui/src/components/form.tsx
@@ -1,94 +1,66 @@
 "use client";
 
-import type { ComponentProps } from "react";
+import type * as LabelPrimitive from "@radix-ui/react-label";
+import type { UseTRPCMutationResult } from "@trpc/react-query/shared";
 import type {
   ControllerProps,
   FieldPath,
   FieldValues,
   UseFormProps,
 } from "react-hook-form";
-import type { ZodType, ZodTypeDef } from "zod";
-import { createContext, use, useId } from "react";
+import type { ZodSchema, z } from "zod";
+import { createContext, useContext, useId } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Slot } from "@radix-ui/react-slot";
 import {
   Controller,
-  useForm as __useForm,
+  FormProvider,
   useFormContext,
+  useForm as useHookForm,
 } from "react-hook-form";
 
+import { Button, buttonVariants } from "@@/ui/button";
+import { Label } from "@@/ui/label";
 import { cn } from "@@/ui/utils";
 
-import { Label } from "./label";
+const Form = FormProvider;
 
-function useForm<
-  TOut extends FieldValues,
-  TDef extends ZodTypeDef,
-  TIn extends FieldValues,
->(
-  props: Omit<UseFormProps<TIn>, "resolver"> & {
-    schema: ZodType<TOut, TDef, TIn>;
-  },
-) {
-  const form = __useForm<TIn, unknown, TOut>({
-    ...props,
-    resolver: zodResolver(props.schema, undefined),
-  });
-
-  return form;
-}
-
-interface FormFieldContextValue<
+type FormFieldContextValue<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-> {
+> = {
   name: TName;
-}
+};
 
-const FormFieldContext = createContext<FormFieldContextValue | null>(null);
+const FormFieldContext = createContext<FormFieldContextValue>(
+  {} as FormFieldContextValue,
+);
 
-function FormField<
+const FormField = <
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
->({ ...props }: ControllerProps<TFieldValues, TName>) {
+>({
+  ...props
+}: ControllerProps<TFieldValues, TName>) => {
   return (
     <FormFieldContext.Provider value={{ name: props.name }}>
       <Controller {...props} />
     </FormFieldContext.Provider>
   );
-}
+};
 
-function useFormField() {
-  const { getFieldState, formState } = useFormContext();
-
-  const fieldContext = use(FormFieldContext);
-  if (!fieldContext) {
-    throw new Error("useFormField should be used within <FormField>");
-  }
-  const fieldState = getFieldState(fieldContext.name, formState);
-
-  const itemContext = use(FormItemContext);
-  const { id } = itemContext;
-
-  return {
-    id,
-    name: fieldContext.name,
-    formItemId: `${id}-form-item`,
-    formDescriptionId: `${id}-form-item-description`,
-    formMessageId: `${id}-form-item-message`,
-    ...fieldState,
-  };
-}
-
-interface FormItemContextValue {
+type FormItemContextValue = {
   id: string;
-}
+};
 
 const FormItemContext = createContext<FormItemContextValue>(
   {} as FormItemContextValue,
 );
 
-function FormItem({ className, ...props }: ComponentProps<"div">) {
+const FormItem = ({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"div">) => {
   const id = useId();
 
   return (
@@ -99,21 +71,51 @@ function FormItem({ className, ...props }: ComponentProps<"div">) {
       />
     </FormItemContext.Provider>
   );
-}
+};
 
-function FormLabel({ className, ...props }: ComponentProps<typeof Label>) {
+const useFormField = () => {
+  const fieldContext = useContext(FormFieldContext);
+  const itemContext = useContext(FormItemContext);
+  const { getFieldState, formState } = useFormContext();
+
+  const fieldState = getFieldState(fieldContext.name, formState);
+
+  // types are mistaken, this is a common issue with React contexts in my experience
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (!fieldContext) {
+    throw new Error("useFormField should be used within <FormField>");
+  }
+
+  const { id } = itemContext;
+
+  return {
+    id,
+    name: fieldContext.name,
+    formItemId: `${id}-form-item`,
+    formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
+    ...fieldState,
+  };
+};
+
+const FormLabel = ({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<typeof LabelPrimitive.Root>) => {
   const { error, formItemId } = useFormField();
 
   return (
     <Label
-      htmlFor={formItemId}
       className={cn(error && "text-destructive", className)}
+      htmlFor={formItemId}
       {...props}
     />
   );
-}
+};
 
-function FormControl({ ...props }: ComponentProps<typeof Slot>) {
+const FormControl = ({
+  ...props
+}: React.ComponentPropsWithRef<typeof Slot>) => {
   const { error, formItemId, formDescriptionId, formMessageId } =
     useFormField();
 
@@ -129,9 +131,12 @@ function FormControl({ ...props }: ComponentProps<typeof Slot>) {
       {...props}
     />
   );
-}
+};
 
-function FormDescription({ className, ...props }: ComponentProps<"p">) {
+const FormDescription = ({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"p">) => {
   const { formDescriptionId } = useFormField();
 
   return (
@@ -141,14 +146,18 @@ function FormDescription({ className, ...props }: ComponentProps<"p">) {
       {...props}
     />
   );
-}
+};
 
-function FormMessage({ className, children, ...props }: ComponentProps<"p">) {
+const FormMessage = ({
+  className,
+  children,
+  ...props
+}: React.ComponentPropsWithRef<"p">) => {
   const { error, formMessageId } = useFormField();
-  const body = error ? String(error.message) : children;
+  const body = error ? String(error.message ?? "") : children;
 
   if (!body) {
-    return;
+    return null;
   }
 
   return (
@@ -160,17 +169,67 @@ function FormMessage({ className, children, ...props }: ComponentProps<"p">) {
       {body}
     </p>
   );
-}
+};
+
+const useForm = <TSchema extends ZodSchema>({
+  schema,
+  ...rest
+}: Omit<UseFormProps<z.infer<TSchema>>, "resolver"> & {
+  schema: TSchema;
+}) => {
+  return useHookForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    ...rest,
+  });
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type TRPCMutation = UseTRPCMutationResult<any, any, any, unknown>;
+
+const FormSubmitButton = ({
+  className,
+  variant,
+  size,
+  children,
+  mutation,
+  disabled,
+  ...props
+}: Omit<React.ComponentProps<typeof Button>, "asChild" | "type"> & {
+  mutation: TRPCMutation;
+}) => {
+  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+  disabled = disabled || mutation.isPending;
+
+  return (
+    <Button
+      type="submit"
+      disabled={disabled}
+      className={cn(
+        buttonVariants({
+          variant,
+          size,
+          className,
+          highlightState: mutation.isError ? "error" : undefined,
+        }),
+      )}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+};
 
 export {
   useForm,
   useFormField,
+  Form,
   FormItem,
   FormLabel,
   FormControl,
   FormDescription,
   FormMessage,
   FormField,
+  FormSubmitButton,
 };
 
-export { FormProvider as Form, useFieldArray } from "react-hook-form";
+export { useFieldArray } from "react-hook-form";
